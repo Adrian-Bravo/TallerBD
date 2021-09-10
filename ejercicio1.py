bd7b310641a57b653e5dd8bdb6867685381410a1
Ivonne-cas <ivonneximenacaste@gmail.com>
2021-09-09 22:35:24 -0500

    primer punto

---
+++ ejercicio1.py
@@ -0,0 +1,15 @@
+
+
+from library.modulos import par_impar
+
+def principal():
+    numero=int (input("Digite un numero"))
+    if par_impar(numero):
+        print("Este numero es par ")
+    else: 
+        print("Este numero es impar")
+
+
+if __name__ == "__main__":
+      principal()
+
---
+++ library/modulos.py
@@ -0,0 +1,6 @@
+def par_impar(num):
+    if num%2 == 0:
+        par = True
+    else:
+        par = False
+    return par
