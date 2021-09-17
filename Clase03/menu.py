ad0001c8d5096f54e000f104bf63076c907f378b
Adrian <acamilobravo@uniguajira.edu.co>
2021-09-16 23:19:37 -0500

    Agregué clase Empleado y heredé la clase Personas

--- Clase03/menu.py
+++ Clase03/menu.py
@@ -27,3 +27,21 @@ class Persona():
         else:
             for i, v in self.personas.items():
                 print(f"{i}: {v}")
+
+
+class Empleado(Persona):
+    def __init__(self):
+        super().__init__()
+
+    def agregarEmpleado(self):
+        super().agregarPersonas()
+        if (self.codigo in self.personas) == False:
+            self.salario = float(input("Salario: "))
+            self.personas[self.codigo] = (self.nombre, self.edad, self.salario)
+
+
+    def eliminarEmpleado(self):
+        super().eliminarPersonas()
+
+    def mostrarEmpleado(self):
+        super().mostrarPersonas()
